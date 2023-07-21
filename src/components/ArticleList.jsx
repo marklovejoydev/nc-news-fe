0d9eb81d7e7553294b7f0489fa0c508844f83d10
marklovejoydev <marklovejoydev@gmail.com>
2023-07-21 11:55:30 +0100

    updated filter to all articles with a hide and show filter button

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,25 +2,48 @@ import React, { useState, useEffect } from 'react'
 import { getArticles } from '../requestApi';
 import { Link } from 'react-router-dom';
 import Loading from './Loading';
+import { useSearchParams } from 'react-router-dom';
+import Expanding from './Expanding';
 
 export default function ArticleList() {
 	const [articles, setArticles] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
-  
+	const [searchParams, setSearchParams] = useSearchParams()
+	const sortByQuery = searchParams.get('sort_by');
+  	const orderQuery = searchParams.get('order');
+
+	const setSortOrder = (sortBy, direction) => {
+		const newParams = new URLSearchParams(searchParams)
+		newParams.set('sort_by', sortBy)
+		newParams.set('order', direction)
+		setSearchParams(newParams)
+	}
+
 	useEffect(() => {
-	  getArticles()
+		getArticles(null, sortByQuery, orderQuery)
 		.then((res) => {
 		  setArticles(res);
 		  setIsLoading(false);
 		})
 		
-	}, []);
+	}, [sortByQuery, orderQuery]);
   
     if (isLoading) return <Loading />
     return (
     
 		<main className='content'>
 			<h2 className='subheader'>ALL ARTICLES</h2>
+			<section>
+    		<Expanding>
+			<div className="spacer"></div>
+    		<button onClick={() => setSortOrder('created_at', 'ASC')}>Sort by Date (ASC)</button>
+      		<button onClick={() => setSortOrder('created_at', 'DESC')}>Sort by Date (DESC)</button>
+      		<button onClick={() => setSortOrder('comment_count', 'ASC')}>Sort by Comment Count (ASC)</button>
+      		<button onClick={() => setSortOrder('comment_count', 'DESC')}>Sort by Comment Count (DESC)</button>
+      		<button onClick={() => setSortOrder('votes', 'ASC')}>Sort by Votes (ASC)</button>
+      		<button onClick={() => setSortOrder('votes', 'DESC')}>Sort by Votes (DESC)</button>
+			</Expanding>
+  			</section>
 			<section>
 				<ul className='content'>
 					{articles.map(({article_id, title, author, topic}) => {
